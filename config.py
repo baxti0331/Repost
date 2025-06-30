cea72f0288a5d52f5ebd1fe19490637fe15ebd67
baxti0331 <sheraliyevbaxtiyor3103@gmail.com>
2025-06-30 06:59:46 +0300

    Update config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-TELEGRAM_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
-SCHEDULER_CHECK_INTERVAL = int(os.getenv("SCHEDULER_CHECK_INTERVAL", 60))  # интервал в секундах
-
-if not TELEGRAM_API_TOKEN:
-    raise ValueError("Переменная окружения TELEGRAM_API_TOKEN не установлена!")
